41b78b7e7f3a349373abd49c4a6c1bc4388acc03
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2025-12-16 10:48:57 +0100

    chore: eliminar servir carpeta public con servidor node

--- main.js
+++ main.js
@@ -22,8 +22,6 @@ async function main() {
     // Middleware to parse JSON
     app.use(express.json());
 
-    // Servidor de archivos estáticos
-    app.use(express.static('public'));
 
     // Metodo Get
     app.get('/users', async (req, res) => {
@@ -40,29 +38,6 @@ async function main() {
       res.json({ message: 'User added successfully', user: newUser });
       console.log("Usuario añadido a la base de datos");
     });
-
-  // Metodo para redirigirme a una pagina u otra 
-  app.get('/', (req, res) => {
-  let fechaSorteo = new Date("2025-09-10T14:00:00");
-  let  fechaActual = new Date();
-  
-  if (fechaActual>fechaSorteo){
-    res.redirect("/GanadorSorteo");
-  } else {
-    res.redirect("/Inscripcion");
-  }
-  });
-
-   // Ruta para la página de Inscripción
-    app.get('/Inscripcion', (req, res) => {
-    res.sendFile(__dirname + '/public/Inscripcion/Inscripcion.html');
-  });
-
-  // Ruta para la página del Ganador del Sorteo
-    app.get('/GanadorSorteo', (req, res) => {
-    res.sendFile(__dirname + '/public/GanadorSorteo/Sorteo.html');
-  });
-
 
 
     // Start server
